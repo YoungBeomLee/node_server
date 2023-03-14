9c56c9d89c43bdd46350e63b02ae9bd78e58524f
YoungBeomLee <edlee127127@gmail.com>
2023-03-14 11:24:23 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+//import React , {} from "react";
+
+const express=require("express");
+const cors=require("cors");
+const app=express();
+const port=8080;
+//json 형식의 데이터를 처리할수 있께 설정
+app.use(express.json());
+app.use()
---
+++ node_index.js
@@ -0,0 +1,27 @@
+var http = require("http");//노드모듈 요청해서 변수http에 저장
+var hostname = "127.0.0.1";
+var port = 8080;
+//서버생성
+const server = http.createServer(function (req, res) {
+  const path = req.url;
+  const method = req.method;
+  if (path === "/products") {
+    if (method === "GET") {
+      res.writeHead(200, { "Content-Type": "application/json" });
+      const products = JSON.stringify([
+        {
+          name: "배변패드",
+          price: 50000,
+        },
+      ]);
+      res.end(products);
+    } else if (method === "POST") {
+      res.end("생성되었습니다!");
+    }
+  }
+  res.end("Good Bye");
+});
+
+server.listen(port, hostname);
+
+console.log("mangoshop server on!");
